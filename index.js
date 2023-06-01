ac5b954f8613abb7e28d08598c5e8ff470924042
pandeypa <114224028+pascalpandey@users.noreply.github.com>
2023-06-02 01:26:46 +0700

    modified:   index.js

--- index.js
+++ index.js
@@ -7,26 +7,71 @@ app.get("/test", (req, res) => {
 });
 
 app.get("/:user/:repo", async (req, res) => {
-  const username = req.params.user;
-  const reponame = req.params.repo;
+  try {
+    const username = req.params.user;
+    const reponame = req.params.repo;
 
-  const repoPath = `C:/${username}/${reponame}`;
+    const repoPath = `C:/${username}/${reponame}`;
 
-  const repo = await nodegit.Repository.open(repoPath);
-  const commit = await repo.getBranchCommit("refs/heads/main");
-  const tree = await commit.getTree()
+    const repo = await nodegit.Repository.open(repoPath);
+    const commit = await repo.getBranchCommit("refs/heads/main");
+    const tree = await commit.getTree();
 
-  const walker = tree.walk();
-  const files = [];
+    const entries = tree.entries();
+    var out = [];
 
-  walker.on("entry", (entry) => {
-    console.log(entry.path())
-    files.push(entry.path());
-  });
+    entries.forEach((entry) => {
+      const path = entry.path();
+      const isDirectory = entry.isDirectory();
 
-  walker.start()
+      out.push({ path, isDirectory });
+    });
 
-  res.send(files)
+    res.send(out);
+  } catch (error) {
+    console.log(error);
+    res.send("invalid path");
+  }
+});
+
+app.get("/:user/:repo/:entrypath(*)", async (req, res) => {
+  try {
+    const username = req.params.user;
+    const reponame = req.params.repo;
+    const entryPath = req.params?.entrypath;
+
+    const repoPath = `C:/${username}/${reponame}`;
+
+    const repo = await nodegit.Repository.open(repoPath);
+    const commit = await repo.getBranchCommit("refs/heads/main");
+    const entry = await commit.getEntry(entryPath);
+
+    if (entry.isTree()) {
+      const tree = await entry.getTree();
+      const entries = tree.entries();
+      var out = [];
+
+      entries.forEach((entry) => {
+        const path = entry.path();
+        const isDirectory = entry.isDirectory();
+
+        out.push({ path, isDirectory });
+      });
+
+      res.send(out);
+    } else if (entry.isFile()) {
+      const fileBlob = await entry.getBlob();
+      var out = fileBlob.toString();
+
+      res.write(out);
+      res.end()
+    } else {
+      var out = "not supported";
+    }
+  } catch (error) {
+    console.log(error);
+    res.send("invalid path");
+  }
 });
 
 const port = process.env.PORT || 8080;
