322fd60103dc9147246a1b97031fc361e065a484
pandeypa <114224028+pascalpandey@users.noreply.github.com>
2023-06-01 23:19:06 +0700

    file reading

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const nodegit = require("nodegit");
+const app = express();
 
 app.get("/test", (req, res) => {
-  res.send("<h1>It's working 🤗</h1>")
-})
+  res.send("<h1>It's working 🤗</h1>");
+});
 
-const port = process.env.PORT || 8080
-app.listen(port, () => console.log(`Listening on port ${port}`))
+app.get("/:user/:repo", async (req, res) => {
+  const username = req.params.user;
+  const reponame = req.params.repo;
+
+  const repoPath = `C:/${username}/${reponame}`;
+
+  const repo = await nodegit.Repository.open(repoPath);
+  const commit = await repo.getBranchCommit("refs/heads/main");
+  const tree = await commit.getTree()
+
+  const walker = tree.walk();
+  const files = [];
+
+  walker.on("entry", (entry) => {
+    console.log(entry.path())
+    files.push(entry.path());
+  });
+
+  walker.start()
+
+  res.send(files)
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}`));
